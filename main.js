59cd3c80ecf0c8556b7ced9e616a4fefdcf27a1b
ashbabayan <ashbabayan16@gmail.com>
2021-03-16 02:59:32 +0400

    fulfill comments, make edit work

--- main.js
+++ main.js
@@ -1,180 +1,195 @@
-var mainContainer = document.getElementById("container")
-var table = document.getElementById("main_list")
-var firstNameInput = document.getElementById("first_name_input")
-var lastNameInput = document.getElementById("last_name_input")
-var cityNameInput = document.getElementById("city_input")
-var addBtn = document.getElementById("add_action")
-var popUp = document.getElementById("addRecordPopup")
-var errorMsg = document.getElementById("error_msg")
-var noDataMsg = document.getElementById("no_data_msg")
-var errorMsg = document.getElementById("error_msg")
-var editedErrorMsg = document.getElementById("edited_error_msg")
-var editPopup = document.getElementById("edit_record")
-var editableFirstNameInput = document.getElementById("editable_first_name")
-var editableLastNameInput = document.getElementById("editable_last_name")
-var editableCityInput = document.getElementById("editable_city")
-
-var idNumber = 0
-function generateId(){
-    var lineId = idNumber +"line"
-    idNumber++
-    return lineId
-}
-
-// use const or let instead of function() use const name = () => {}
-function noData(){ // change name to toggleErrorMessage -> anuny piti lini bay
-    if (table.children.length > 1) {
-        noDataMsg.classList.add("hidden")
-    } else if (table.children.length == 1 && noDataMsg.classList.contains("hidden")) {
-        noDataMsg.classList.remove("hidden")
-    }
-}
-
-function clearInputValues(){ // anuny dzel, ban chi asum
-    firstNameInput.value = ""
-    lastNameInput.value = ""
-    cityNameInput.value = ""
-}
-
-function openPopup(){ // anuny dzel, ban chi asum
-    popUp.classList.remove("hidden")
-    popUp.classList.add("flex")
-    mainContainer.style.opacity = "0.1"
-}
-
-function closePopUp(){ // tabs, dzi, anuny dzel
-    if(popUp.classList.contains("flex")){
-        popUp.classList.add("hidden")
-        popUp.classList.remove("flex")
-    }
-    mainContainer.style.opacity= "1"
-    clearInputValues()
-    if(errorMsg.classList.contains("hidden")){
-        return true
-    }else {
-    errorMsg.classList.add("hidden")
-    }
-}
-
-function closeEditPopup(){ // tabs, dzi
-    if(editPopup.classList.contains("flex")){
-        editPopup.classList.add("hidden")
-        editPopup.classList.remove("flex")
-    }
-    mainContainer.style.opacity= "1"
-    clearInputValues()
-    if(editedErrorMsg.classList.contains("hidden")){
-        return true
-    }else {
-    editedErrorMsg.classList.add("hidden")
-    }
-}
-
+var mainContainer = document.getElementById("container");
+var table = document.getElementById("main_list");
+var tableBody = document.getElementById("table_body");
+var firstNameInput = document.getElementById("first_name_input");
+var lastNameInput = document.getElementById("last_name_input");
+var cityNameInput = document.getElementById("city_input");
+var addBtn = document.getElementById("add_action");
+var popUp = document.getElementById("add_record_popup");
+var invalidAddErrorMsg = document.getElementById("validation_adding_error_message");
+var noDataMsg = document.getElementById("no_data_msg");
+var invalidEditErrorMsg = document.getElementById("validation_editing_error_message");
+var editPopup = document.getElementById("edit_record_popup");
+var editableFirstNameInput = document.getElementById("editable_first_name");
+var editableLastNameInput = document.getElementById("editable_last_name");
+var editableCityInput = document.getElementById("editable_city");
+var searchInput = document.getElementById("searchbox")
+var numbers = /\d/
 const d = new Date();
-var data = []
+var data = [];
+var userIndex;
 
-function openEditPopup(){
-    //open edit popup
-    editPopup.classList.remove("hidden");
-    editPopup.classList.add("flex");
-    mainContainer.style.opacity = "0.1";
-    var rowIndex = (Array.prototype.indexOf.call(table.children, event.target.parentElement.parentElement)) // <- ahavor koda, poxel
-    // fill inputs with correct values
-    editableFirstNameInput.value = data[rowIndex -1].firstname
-    editableLastNameInput.value = data[rowIndex -1].lastname
-    editableCityInput.value = data[rowIndex -1].city
+const checkAddRecordValidation = () => {
+    return (!(firstNameInput.value && lastNameInput.value && cityNameInput.value))
 }
 
-function createEditButton(index){ // use const instead, BOLOR texerum
-    var editButton = document.createElement("button")
-    editButton.innerHTML = '<i class="fa fa-edit"></i>'
-    editButton.classList.add("actions_style")
-    editButton.setAttribute("id", 'edit_action');
-    editButton.addEventListener("click", (e) => {
-        console.log(index);
-    })
-    //function for clicking on edit button
+const checkNumberValidation = () => {
+    return ((numbers.test(firstNameInput.value) || numbers.test(lastNameInput.value) || numbers.test(cityNameInput.value)))
+}
+
+const checkEditRecordValidation = () => {
+    return (!(editableFirstNameInput.value && editableLastNameInput.value && editableCityInput.value))
+}
+
+const checkEditedNumberValidation = () => {
+    return (numbers.test(editableFirstNameInput.value) || numbers.test(editableLastNameInput.value) || numbers.test(editableCityInput.value))
+}
+
+const toggleEmptyDataMsg = () => {
+    if (table.rows.length > 1) {
+        noDataMsg.classList.add("hidden");
+    } else if (table.rows.length == 1 && noDataMsg.classList.contains("hidden")) {
+        noDataMsg.classList.remove("hidden");
+    }
+}
+
+const clearInputValues = () => {
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+    cityNameInput.value = "";
+}
+
+const openAddRecordPopup = () => {
+    popUp.classList.remove("hidden");
+    popUp.classList.add("popup_style");
+    mainContainer.style.opacity = "0.1";
+}
+
+const closeAddRecordPopUp = () => {
+    if (popUp.classList.contains("popup_style")) {
+        popUp.classList.add("hidden");
+        popUp.classList.remove("popup_style");
+    }
+        mainContainer.style.opacity= "1";
+        clearInputValues();
+    if (invalidAddErrorMsg.classList.contains("hidden")) {
+        return true
+    } else {
+        invalidAddErrorMsg.classList.add("hidden")
+    }
+}
+
+const openEditPopup = () => {
+    editPopup.classList.remove("hidden");
+    editPopup.classList.add("popup_style");
+    mainContainer.style.opacity = "0.1";
+}
+
+const closeEditPopup = () => {
+    if (editPopup.classList.contains("popup_style")) {
+        editPopup.classList.add("hidden");
+        editPopup.classList.remove("popup_style");
+    }
+    mainContainer.style.opacity= "1";
+    clearInputValues();
+    if (invalidEditErrorMsg.classList.contains("hidden")) {
+        return true;
+    } else {
+        invalidEditErrorMsg.classList.add("hidden");
+    }
+}
+
+const createEditButton = (index) => {
+    var editButton = document.createElement("button");
+        editButton.innerHTML = '<i class="fa fa-edit"></i>';
+        editButton.classList.add("actions_style");
+        editButton.setAttribute("id", 'edit_action');
+        editButton.addEventListener("click", (e) => {
+            openEditPopup();
+            editableFirstNameInput.value = data[index].firstname;
+            editableLastNameInput.value = data[index].lastname;
+            editableCityInput.value = data[index].city;
+            userIndex = index;
+        })
     return editButton
 }
 
-function createDeleteButton(){
-    var deleteButton = document.createElement("button")
-    deleteButton.innerHTML = '<i class="fa fa-trash">'
-    deleteButton.classList.add("actions_style")
-    deleteButton.setAttribute("id",'delete_action')
-    deleteButton.addEventListener("click", function(){
-
-    })
+const createDeleteButton = (index) => {
+    var deleteButton = document.createElement("button");
+        deleteButton.innerHTML = '<i class="fa fa-trash">';
+        deleteButton.classList.add("actions_style");
+        deleteButton.setAttribute("id",'delete_action');
+        deleteButton.addEventListener("click", (e) => {
+            data.splice(index, 1)
+            render()
+        })
     return deleteButton 
 }
 
-function createActionsColumn(index){
-    var deleteButton = createDeleteButton()
-    var editButton = createEditButton(index)
-    var actions = document.createElement("td")
-    actions.classList.add("actions")
-    actions.appendChild(editButton)
-    actions.appendChild(deleteButton)
+const createActionsColumn = (index) => {
+    var deleteButton = createDeleteButton(index);
+    var editButton = createEditButton(index);
+    var actions = document.createElement("td");
+        actions.classList.add("actions");
+        actions.appendChild(editButton);
+        actions.appendChild(deleteButton);
     return actions
 }
 
-function createDate(){
-    var date = document.createElement("td")
-        date.innerHTML = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDay()
-        return date
+const createDate = () => {
+    var date = document.createElement("td");
+        date.innerHTML = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDay();
+    return date
 }
-function createTime(){
-    var time = document.createElement("td")
-    time.innerHTML = d.getHours() + ":" + d.getMinutes()
+
+const createTime = () => {
+    var time = document.createElement("td");
+        time.innerHTML = d.getHours() + ":" + d.getMinutes();
     return time
 }
 
-function createUserRow(){
-    var userRow = document.createElement("tr")
-        userRow.classList.add("font") // anuny poxel
-        var fullNameCell = document.createElement("td")
-        var cityNameCell = document.createElement("td")
-        data.forEach((row, index) => {
-            console.log(index);
-            fullNameCell.innerHTML = row.firstname + " " + row.lastname
-            cityNameCell.innerHTML = row.city
-            userRow.appendChild(fullNameCell)
-            userRow.appendChild(cityNameCell)
-
-            var date = createDate()
-            var time = createTime()
-            var actions = createActionsColumn(index)
-            userRow.appendChild(date)
-            userRow.appendChild(time)
-            userRow.appendChild(actions)
-        })
-
-        return userRow
+const createUserRow = () => {
+    var userRow = document.createElement("tr");
+        userRow.classList.add("user_record");
+    return userRow
 }
 
-function render(){
-    while(table.rows.length>1){ 
-        table.deleteRow(1)
+const render = () => {
+    while (table.rows.length > 1) { 
+        table.deleteRow(1);
     }
-    var userRow = createUserRow()
-    table.appendChild(userRow)
-    
-  }
+    data.forEach((row, index) => {
+        var userRow = createUserRow()
+        var fullNameCell = document.createElement("td");
+        var cityNameCell = document.createElement("td");
+        var date = createDate();
+        var time = createTime();
+        var actions = createActionsColumn(index);
+        fullNameCell.innerHTML = row.firstname + " " + row.lastname;
+        cityNameCell.innerHTML = row.city;
+        userRow.appendChild(fullNameCell);
+        userRow.appendChild(cityNameCell);
+        userRow.appendChild(date);
+        userRow.appendChild(time);
+        userRow.appendChild(actions);
+        tableBody.appendChild(userRow);
+    });
+    toggleEmptyDataMsg()
+}
 
-  const addUser = () => {
+const addUser = () => {
     var user = { 
         firstname: firstNameInput.value,
         lastname: lastNameInput.value,
         city: cityNameInput.value
     }
-
+    if (checkAddRecordValidation() || checkNumberValidation()) {
+        invalidAddErrorMsg.classList.remove("hidden")
+        return
+    }
     data.push(user);
-    console.log(data);
-    render()
-    noData() // anuny poxel -> bay (voch goyakan)
-    closePopUp()
+    render();
+    closeAddRecordPopUp();
 }
 
 const editRecord = () => {
-    
+    if(checkEditRecordValidation() || checkEditedNumberValidation()){
+        invalidEditErrorMsg.classList.remove("hidden")
+        return
+    }
+    data[userIndex].firstname = editableFirstNameInput.value;
+    data[userIndex].lastname = editableLastNameInput.value;
+    data[userIndex].city = editableCityInput.value;
+    render();
+    closeEditPopup()
 }
